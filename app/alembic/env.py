fc6ee5f77802cfbdcecaaf4990533531f3175368
Eslam <41278977+eslam5464@users.noreply.github.com>
2026-01-22 20:30:57 +0200

    Import all models in Alembic environment for migration support

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -8,6 +8,7 @@ from sqlalchemy.sql.ddl import CreateSchema
 
 from app.core.config import settings
 from app.core.db import meta
+from app.models import *  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
