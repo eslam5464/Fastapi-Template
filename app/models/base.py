95085636de0322d6a8cd89693548136c421912c1
Eslam <41278977+eslam5464@users.noreply.github.com>
2026-02-19 06:13:35 +0200

    Add methods to retrieve schema, table name, and column keys in Base model

--- app/models/base.py
+++ app/models/base.py
@@ -37,6 +37,13 @@ class Base(DeclarativeBase):
     ) -> dict[str, Any]:
         """
         Convert the model instance to a dictionary with only serializable fields.
+
+        Args:
+            exclude_keys (set[str], optional): A set of field names to exclude from the resulting dictionary. Defaults to None.
+            exclude_none (bool, optional): If True, fields with None values will be excluded from the resulting dictionary. Defaults to False.
+
+        Returns:
+            serialized_data (dict[str, Any]): A dictionary representation of the model instance with only serializable fields.
         """
         exclude_keys = exclude_keys or set()
         serialized_data = {}
@@ -53,3 +60,42 @@ class Base(DeclarativeBase):
             serialized_data[key] = value
 
         return serialized_data
+
+    @classmethod
+    def get_schema(cls) -> Optional[str]:
+        """
+        Get the schema name for the model.
+
+        Returns:
+            schema_name (str | None): The schema name if available, otherwise None.
+        """
+        return cls.metadata.schema if cls.metadata else None
+
+    @classmethod
+    def get_table_name(cls) -> Optional[str]:
+        """
+        Get the table name for the model.
+
+        Returns:
+            table_name (str | None): The table name if available, otherwise None.
+        """
+        return cls.__tablename__ if hasattr(cls, "__tablename__") else None
+
+    @classmethod
+    def dict_keys(cls) -> set[str]:
+        """
+        Get the set of column keys for the model.
+
+        Returns:
+            keys (set[str]): A set of column keys for the model.
+        """
+        return set(class_mapper(cls).c.keys())
+
+    def __repr__(self) -> str:
+        """
+        Get the string representation of the model instance.
+
+        Returns:
+            repr_str (str): The string representation of the model instance.
+        """
+        return f"<{self.__class__.__name__}(id={self.id})>"
